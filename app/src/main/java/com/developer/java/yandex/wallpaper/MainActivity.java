993ce6a38bb81ebb172d2df759c709f1bb3e73c1
Shiplayer <shiplayer@yandex.ru>
2018-08-01 19:31:50 +0300

    create xml, declare recycleView

--- app/src/main/java/com/developer/java/yandex/wallpaper/MainActivity.java
+++ app/src/main/java/com/developer/java/yandex/wallpaper/MainActivity.java
@@ -1,13 +1,47 @@
 package com.developer.java.yandex.wallpaper;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int PERMISSION_CODE = 1001;
+    private boolean mPermissionIsGranted = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init() {
+        RecyclerView recyclerView = findViewById(R.id.rv_wallpapers);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSION_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case PERMISSION_CODE:{
+                if(grantResults.length > 0){
+                    if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                        mPermissionIsGranted = true;
+                    } else {
+                        mPermissionIsGranted = false;
+                    }
+                }
+                break;
+            }
+        }
     }
 }
